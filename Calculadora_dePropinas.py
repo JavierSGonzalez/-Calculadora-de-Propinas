5b0861d829fb1329ea2173bee728476b45f27b53
tomasBONILL <tomasedubonilla@gmail.com>
2025-02-18 08:00:19 -0600

    Interfaz mejorada

--- Calculadora_dePropinas.py
+++ Calculadora_dePropinas.py
@@ -1,17 +1,28 @@
 historial = []
- 
+
 while True:
     print("Calcular propina: \n")
- 
+
+    print("¿Que quieres hacer?")
+    print("Ir a calculadora(1)  Salir(2)")
+    while True:
+        res = input()
+        if res == "1":
+            break
+        elif res == "2":
+            print("Muchas gracias por usar la calculadora de propinas")
+        else:
+            print("Escribe una opción valida")
+
     print("Monto total de la cuenta - Escribe solo el número")
     i = float(input())  
     print(f"${i} \n")
- 
+
     print("Porcentaje de propina que desea agregar, escriba '=' y el porcentaje")
     print("=10%")
     print("=15%")
     print("=20%")
- 
+
     while True:
         pro = input()
         if pro == "=10%":
@@ -25,23 +36,23 @@ while True:
             break
         else:
             print("Usa el formato correcto: =10%, =15% o =20% \n")
- 
+
     propinaA = round(propina, 2)
     print(f"Propina: ${propinaA} \n")
- 
+
     print("Personas a pagar")
     pe = float(input())
-   
+
     tot = (i+propinaA) / pe
     totA= round(tot, 2)
- 
+
     historial.append(totA)
- 
+
     print(f"Total equivalente por persona: ${totA} \n")
     print(f"Historial de totales equivalentes: $ {historial}")
     for tot in historial:
         print(f"${totA}")
-   
+
     print("¿Quieres calcular otra propina? Responde s/n:")
     while True:
         salir = input()
